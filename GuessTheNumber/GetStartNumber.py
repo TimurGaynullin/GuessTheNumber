bfecf6fd0587e50660220b8ccb90b3f2fa51996e
NiketaPopov <nikitakazan0717n@gmail.com>
2025-02-12 19:53:01 +0300

    стартовое число типо да

--- GuessTheNumber/GetStartNumber.py
+++ GuessTheNumber/GetStartNumber.py
@@ -5,8 +5,19 @@
 2. Если пользователь ввел не число или отрицательное число, то вывести ошибку и попросить пользователя ввести число еще раз.
 3. Если число подходит, то вернуть его.
 '''
-# Это задание выполнит ...
+from GuessTheNumber.IsPositiveNumber import IsPositiveNumber
+
+
+# Это задание выполнит Никита
 
 
 def GetStartNumber():
-    print('GetStartNumber')
+    print('Введите стартовое число:')
+    StartNumber = input()
+    if IsPositiveNumber(StartNumber):
+        return int(StartNumber)
+    else:
+        print('Введено неверное значение. Возможно вы ввели текст или отрицательное число, введите положительное число и повторите попытку')
+        return GetStartNumber()
+
+GetStartNumber()
