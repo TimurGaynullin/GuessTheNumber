9f79a0590156d96107b130a741ab9701ade3e44c
amnyam3 <akmalllmingazov@gmail.com>
2025-02-12 20:27:04 +0300

    сделал Play

--- GuessTheNumber/Play.py
+++ GuessTheNumber/Play.py
@@ -5,8 +5,29 @@
 3. Если введено не число, или не в том промежутке, или не верное, дать подсказку и предложить ввести число еще раз.
 4. Если число угадано, завершить выполнение функции.
 '''
+from GiveHint import GiveHint
+from IsNumberInSegment import IsNumberInSegment
+from IsPositiveNumber import IsPositiveNumber
+
+
 # Это задание выполнит Акмаль
 
 
-def Play():
-    print('Play')
+def Play(start, end, answer):
+    IsVictory = False
+    print(f'Мы загадали число от {start} до {end}. Отгадайте его!')
+    while not IsVictory:
+        print('Какое число мы загадали?')
+        number = input()
+        if IsPositiveNumber(number):
+            number = int(number)
+            if IsNumberInSegment(start, end, answer):
+                if number == answer:
+                    IsVictory = True
+                else:
+                    GiveHint(number, answer)
+            else:
+                print(f'Это число не входит в отрезок от {start} до {end}. Попробуйте еще раз')
+
+        else:
+            print('Необходимо ввести положительное число (без знака минус и других символов). Попробуйте еще раз')
