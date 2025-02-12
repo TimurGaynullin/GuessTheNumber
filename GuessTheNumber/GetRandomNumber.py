ef9622d3fb4ed67fdfafc541517c8faaa994a1ff
RONawht <aronmelesenko339@gmail.com>
2025-02-12 19:55:53 +0300

    my personality realized and make a feture GetRandomNumber

--- GuessTheNumber/GetRandomNumber.py
+++ GuessTheNumber/GetRandomNumber.py
@@ -3,7 +3,7 @@
 А затем вернуть случайное целое число в промежутке от начального до конечного.
 Как получить случайное число, можно узнать в интернете
 '''
-# Это задание выполнит ...
-
-def GetRandomNumber():
-    print('GetRandomNumber')
+# Это задание выполнит RONawht
+import random
+def GetRandomNumber(first_number, ended_number):
+    return random.randint(first_number, ended_number)
