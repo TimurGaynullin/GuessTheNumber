ef7a182fd6ee4043ddaa8c09cf8428d1c690317f
RONawht <aronmelesenko339@gmail.com>
2025-02-12 20:26:44 +0300

    доделал мейн

--- GuessTheNumber/main.py
+++ GuessTheNumber/main.py
@@ -4,7 +4,14 @@
 Запустить игру.
 Поздравить пользователя с победой.
 '''
-# Это задание выполнит ...
+# Это задание выполнит страдалец RONawht
 
+from GuessTheNumber.GetRandomNumber import GetRandomNumber
+from GuessTheNumber.GetRange import GetRange
+from GuessTheNumber.Play import Play
 
-print('Старт проекта GuessTheNumber')
+(start, end) = GetRange()
+random_number = GetRandomNumber(start, end)
+Play(start, end, random_number)
+
+print('ТЫ МОЛОДЕЦ!!!')
