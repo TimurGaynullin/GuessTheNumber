9e295c78a7575bf354826bfc157d672c4b3ce102
Aisylu2007 <A.Foenko2007@yandex.ru>
2025-02-12 20:02:22 +0300

    Сделала функцию IsPositiveNumber

--- GuessTheNumber/IsPositiveNumber.py
+++ GuessTheNumber/IsPositiveNumber.py
@@ -3,8 +3,22 @@
 Если да, то вернуть True.
 Если нет, то вернуть False.
 '''
-# Это задание выполнит ...
+#Это задание выполнит Айсылу
+string = input()
+def IsPositiveNumber(string):
+    counof0 = string.count('0')
+    countof1 = string.count('1')
+    countof2 = string.count('2')
+    countof3 = string.count('3')
+    countof4 = string.count('4')
+    countof5 = string.count('5')
+    countof6 = string.count('6')
+    countof7 = string.count('7')
+    countof8 = string.count('8')
+    countof9 = string.count('9')
+    sumcounts = countof0 + countof1 + countof2 + countof3 + countof4 + countof5 + countof6 +countof7 + countof8 + countof9
+    if len(string) == sumcounts and string[0] != '0':
+        return True
+    else:
+        return False
 
-
-def IsPositiveNumber():
-    print('IsPositiveNumber')
